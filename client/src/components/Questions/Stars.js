51e685738a989cf47e5816f7062dd4afa450ae49
smiley-coyote <nick.a.alex@gmail.com>
2018-10-05 20:39:50 -0400

    finally able to add rating with movieid. need to add api db call next

--- client/src/components/Questions/Stars.js
+++ client/src/components/Questions/Stars.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./Stars.css"
 
 export const Stars = props => (
-<fieldset className="rating">
+<div>
     <h3>Please rate:</h3>
-    <input type="radio" id="star5" name="rating" value="5" /><label htmlFor="star5" title="Rocks!"></label>
-    <input type="radio" id="star4" name="rating" value="4" /><label htmlFor="star4" title="Pretty good"></label>
-    <input type="radio" id="star3" name="rating" value="3" /><label htmlFor="star3" title="Meh"></label>
-    <input type="radio" id="star2" name="rating" value="2" /><label htmlFor="star2" title="Kinda bad"></label>
-</fieldset>
+    <input type="button" onClick={(evt) => props.handleRatingInputChange(props.imdbID, evt)} id="star4" name="rating" value="4" /><label htmlFor="star4"></label>
+    <input type="button" onClick={(evt) => props.handleRatingInputChange(props.imdbID, evt)} id="star3" name="rating" value="3" /><label htmlFor="star3"></label>
+    <input type="button" onClick={(evt) => props.handleRatingInputChange(props.imdbID, evt)} id="star2" name="rating" value="2" /><label htmlFor="star2"></label>
+    <input type="button" onClick={(evt) => props.handleRatingInputChange(props.imdbID, evt)} id="star1" name="rating" value="1" /><label htmlFor="star1"></label>
+</div>
 )
--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -5,18 +5,35 @@ import { push } from 'react-router-redux';
 import SearchForm from "../../components/SearchForm"
 import Wrapper from "../../components/Wrapper"
 import Results from "../../components/Results"
+import "./Search.css"
+
 
 
 
 
 class Search extends Component {
   state = {
-  movieSearch: "",
-  results: [],
-  rating: 0
+    movieSearch: "",
+    results: [],
+    rating: 0,
+    title: 0,
+  };
+
+  handleRatingInputChange = event => {
+    event.preventDefault();
+    const thisid = event.currentTarget.name
+    const value = event.target.htmlFor
+    console.log(thisid)
+    console.log(value);
+
+    this.setState({
+      title: thisid
+    })
+
   };
 
   handleInputChange = event => {
+    event.preventDefault();
     const { name, value } = event.target;
     this.setState({
       [name]: value
@@ -26,44 +43,50 @@ class Search extends Component {
 
   handleFormClear = event => {
     event.preventDefault();
-    this.setState({results: []})
+    this.setState({ results: [] })
   }
 
   handleFormSubmit = event => {
     event.preventDefault();
     API.movieSearch(this.state.movieSearch)
-    .then(res =>{
-      console.log(res.data.Search);
-      this.setState({ results: res.data.Search})
-    })
+      .then(res => {
+        console.log(res.data.Search);
+        this.setState({ results: res.data.Search })
+      })
   };
 
 
   render() {
     return (
       <Wrapper>
-      <SearchForm
-      value={this.state.search}
-      handleInputChange={this.handleInputChange}
-      handleFormSubmit={this.handleFormSubmit}
-      handleFormClear={this.handleFormClear} 
-      >
-      </SearchForm>
-      <Results>
-    {this.state.results.map(result => (
-         <div key={result._id}>
-         <img src={result.Poster} alt={result.Title}/>
-         <br />
-         <h2>{result.Title}</h2>
-         <p>{result.Year}</p>
-         <Stars />
-         <hr />
-         <br />
-         <br />
-         <br />
-         </div>
-    ))}
- </Results>
+        <p>{this.state.title}</p>
+        <SearchForm
+          value={this.state.movieSearch}
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+          handleFormClear={this.handleFormClear}
+        >
+        </SearchForm>
+
+        <Results>
+          {this.state.results.map(result => (
+            <div key={result.imdbID}>
+              <img src={result.Poster} alt={result.Title} />
+              <br />
+              <h2>{result.Title}</h2>
+              <p>{result.Year}</p>
+              <p>{result.imdbID}</p>
+              <fieldset className="rating" name={result.imdbID} onClick={this.handleRatingInputChange}>
+                <h3>Please rate:</h3>
+                <input type="radio" id="star4" name="rating" value="4" /><label htmlFor="4"></label>
+                <input type="radio" id="star3" name="rating" value="3" /><label htmlFor="3"></label>
+                <input type="radio" id="star2" name="rating" value="2" /><label htmlFor="2"></label>
+                <input type="radio" id="star1" name="rating" value="1" /><label htmlFor="1"></label>
+              </fieldset>
+            </div>
+          ))}
+        </Results>
+
 
       </Wrapper>
     )
