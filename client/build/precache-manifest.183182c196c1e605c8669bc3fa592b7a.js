4636df25ca109e11620e06a49dc550e9d6518183
smiley-coyote <nick.a.alex@gmail.com>
2018-10-18 20:50:31 -0400

    installed packages

---
+++ client/build/precache-manifest.183182c196c1e605c8669bc3fa592b7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ded2e7294e2de26e28bd",
+    "url": "/static/js/main.ded2e729.chunk.js"
+  },
+  {
+    "revision": "620b06f03b861a395d11",
+    "url": "/static/js/1.620b06f0.chunk.js"
+  },
+  {
+    "revision": "ded2e7294e2de26e28bd",
+    "url": "/static/css/main.4178d6d1.chunk.css"
+  },
+  {
+    "revision": "32acc52d6ca4de9a40cf71fdc49cd6d2",
+    "url": "/index.html"
+  }
+];
